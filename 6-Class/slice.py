344f4997dbee82c14d1a9897edc6260381e1a3aa
senthil <senthil.veeranan@gmail.com>
2020-09-04 02:43:39 +0530

    6-class added

--- 6-Class/slice.py
+++ 6-Class/slice.py
@@ -13,4 +13,12 @@ print(s[:])
 print (s[:6])
 
 #s[begin:end:step]
-# the default value of step value is 1
+# begin alsways starts from 0(zero) end value always (end-1)
+# the default value of step value is 1  
+# example:
+s2='abcedfghijklmnopqrtuvwxyz'
+s2[2:15]
+#Here 2 means c and 8 means 7 , 7 is h so the answer would be cdefgh
+s2[2:15:2]
+s2[2:15:3]
+
