1def5d056bf14458cdea84616d1bfec973db6067
senthil <senthil.veeranan@gmail.com>
2020-09-04 06:14:07 +0530

    file added

---
+++ 1/8-Class/list.py
@@ -0,0 +1,65 @@
+list:
+When we go for list data type ?
+if you want to represent a group of objects as a single entity where 
+insertion orser is preserved and 
+duplicates are allowd 
+Any type of value like init,str,float No problem at all 
+
+Ex:
+
+l=[10,20,'senthil',10,30,True]
+print(l[0])
+print(l[1])
+print(l[-1])
+print(l)
+
+
+l=[]
+l.append(10)
+l.append(20)
+l.append('senthilveeranan')
+l.append(None)
+l.append(True)
+l.append(10)
+l.append('sivasakthithunai')
+l.append(123.456)
+print(l)
+print(id(0))
+print(id(5))
+print(len(l))
+print(id(l))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
